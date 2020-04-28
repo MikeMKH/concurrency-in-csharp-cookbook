59644800849963e4dad4a6f235e13fa6119a61c7
Mike Harris <MikeMKH@users.noreply.github.com>
2020-04-28 06:26:53 -0500

    add example 5.4

--- ch05/Example/Examples.cs
+++ ch05/Example/Examples.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks.Dataflow;
 using Xunit;
 
@@ -8,7 +7,7 @@ namespace Example
     public class Examples
     {
         [Fact]
-        public void TestLinkTo()
+        public void TestLinkToAssert()
         {
             var t1 = new TransformBlock<int, int>(x => x + 2);
             var t2 = new TransformBlock<int, int>(x => x * 2);
@@ -24,6 +23,29 @@ namespace Example
             t1.Complete();
             
             test.Completion.Wait();
+        }
+        
+        [Fact]
+        public void TestBoundedCapacity()
+        {
+            var t1 = new TransformBlock<int, int>(x => x + 2);
+            var t2 = new TransformBlock<int, string>(x => (x * 2).ToString());
+            var b = new BufferBlock<string>(new DataflowBlockOptions { BoundedCapacity = 1 });
+            
+            var log = new TransformBlock<string, string>(x => { Console.WriteLine($"TransformBlock has {x}"); return x; });
+            var test = new ActionBlock<string>(x => Assert.Equal("8", x));
+            
+            var options = new DataflowLinkOptions { PropagateCompletion = true };
+            t1.LinkTo(t2, options);
+            t2.LinkTo(b, options);
+            b.LinkTo(log, options);
+            log.LinkTo(test, options);
+            
+            
+            t1.Post(2);
+            t1.Complete();
+            
+            b.Completion.Wait();
         }
     }
 }
