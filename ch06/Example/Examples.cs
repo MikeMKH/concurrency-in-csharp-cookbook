55ad39cdf66b447ea116d40d4bb7f3bb24ca6e39
Mike Harris <MikeMKH@users.noreply.github.com>
2020-05-05 06:17:02 -0500

    add examples 6.3 - 6.6

--- ch06/Example/Examples.cs
+++ ch06/Example/Examples.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Reactive;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-using System.Reactive.Threading.Tasks;
-using System.Threading;
 using System.Timers;
 using Timer = System.Timers.Timer;
 using Xunit;
@@ -26,7 +22,40 @@ namespace Example
             );
             ticks
               .Select(_ => counter++)
-              .Subscribe(data => Console.WriteLine($"Timer: {counter}"));
+              .Subscribe(_ => Console.WriteLine($"Timer\t:{counter}"));
+        }
+        
+        [Fact]
+        public void ExampleTimerCanBeObservableWithReflection()
+        {
+            var counter = 0;
+            var timer = new System.Timers.Timer(interval: 10) { Enabled = true };
+            IObservable<EventPattern<object>> ticks =
+            Observable.FromEventPattern(timer, nameof(Timer.Elapsed));
+            ticks
+              .Select(_ => counter++)
+              .Subscribe(_ => Console.WriteLine($"Reflect\t:{counter}"));
+        }
+        
+        [Fact]
+        public void ExampleBuffer()
+        {
+            Observable.Interval(TimeSpan.FromMilliseconds(10))
+              .Buffer(2)
+              .Subscribe(x => Console.WriteLine($"Buffer\t:{x[0]}, {x[1]}"));
+        }
+        
+        [Fact]
+        public void ExampleWindow()
+        {
+            Observable.Interval(TimeSpan.FromMilliseconds(10))
+              .Window(2)
+              .Subscribe(group => {
+                  Console.WriteLine($"Window\t:Starting Group");
+                  group.Subscribe(
+                      x => Console.WriteLine($"Window\t:{x}"),
+                      () => Console.WriteLine($"Window\t:Ending Group"));
+              });
         }
     }
 }
