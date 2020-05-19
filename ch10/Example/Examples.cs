da343eb75ff98921a24d9d87ee8fe8b9bb2f27b1
Mike Harris <MikeMKH@users.noreply.github.com>
2020-05-19 06:36:43 -0500

    add examples 10.1 - 10.3

--- ch10/Example/Examples.cs
+++ ch10/Example/Examples.cs
@@ -15,11 +15,44 @@ using Xunit;
 namespace Example
 {
     public class Examples
-    {
+    {   
         [Fact]
-        public void Test1()
+        public void ExampleIssueCancelRequest()
         {
-
+            using var cts = new CancellationTokenSource();
+            var task = CancelableMethodAsync("ExampleIssueCancelRequest", cts.Token);
+            
+            cts.Cancel();
+            
+            async Task<int> CancelableMethodAsync(
+                string message, CancellationToken cancellationToken = default)
+            {
+                Console.WriteLine($"CancelableMethodAsync: START {message}");
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                Console.WriteLine($"CancelableMethodAsync: END   {message}");
+                return 8;
+            }
+        }
+        
+        [Fact]
+        public void TestCancelableMethodThrowIfCancellationRequested()
+        {
+            using var cts = new CancellationTokenSource();
+            Assert.ThrowsAsync<OperationCanceledException>(async () =>
+            {
+                var task = CancelableMethodAsync(cts.Token);
+                cts.Cancel();
+            });
+            
+            async Task<int> CancelableMethodAsync(CancellationToken cancellationToken)
+            {
+                foreach (var x in Enumerable.Range(1, 100))
+                {
+                    Thread.Sleep(1);
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+                return 8;
+            }
         }
     }
 }
