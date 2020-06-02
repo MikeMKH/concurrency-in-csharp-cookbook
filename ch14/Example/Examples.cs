769ef3265c3e696b01d89f4d1dfa7a574444d5e2
Mike Harris <MikeMKH@users.noreply.github.com>
2020-06-02 06:22:05 -0500

    add examples 14.1 - 14.3

--- ch14/Example/Examples.cs
+++ ch14/Example/Examples.cs
@@ -15,10 +15,55 @@ namespace Example
 {
     public class Examples
     {
-        [Fact]
-        public void Test1()
+        class SharedFoo
         {
-
+            static int _value;
+            static readonly Lazy<int> SharedValue = new Lazy<int>(() =>
+            {
+                Console.WriteLine($"Setting SharedValue to {_value + 1}");
+                return _value++;
+            });
+            
+            public int UseAndReturnSharedValue()
+            {
+                int value = SharedValue.Value;
+                return value;
+            }
+        }
+        [Fact]
+        public void TestUsingSharedValueReturnsSameValue()
+        {
+            var sut = new SharedFoo();
+            var expected = sut.UseAndReturnSharedValue();
+            var acutal = sut.UseAndReturnSharedValue();
+            Assert.Equal(expected, acutal);
+        }
+        
+        [Fact]
+        public async void TestSubscribeWithDeferAsync()
+        {
+            List<Task<int>> spies = new List<Task<int>>();
+            var observable = Observable.Defer(() =>
+            {
+                var t = GetValueAsync();
+                spies.Add(t);
+                return t.ToObservable();
+            });
+            
+            int value1 = 0, value2 = 0;
+            observable.Subscribe(x => value1 = x);
+            observable.Subscribe(x => value2 = x);
+            
+            Task.WaitAll(spies.ToArray());
+            Assert.Equal(value1, value2);
+            
+            async Task<int> GetValueAsync()
+            {
+                Console.WriteLine($"START GetValueAsync on {Task.CurrentId}");
+                await Task.Delay(TimeSpan.FromMilliseconds(100));
+                Console.WriteLine($"END   GetValueAsync on {Task.CurrentId}");
+                return 8;
+            }
         }
     }
 }
